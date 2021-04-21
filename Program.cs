f6e6aa9312ab6260eed90463435ba6e923c9111e
Atolagbe Muiz <atolagbemuiz@gmail.com>
2021-04-21 12:54:02 +0100

    Latest commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,90 @@ namespace Control_Statements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // int number = 10;
+            // if(number == 10)
+            // {
+            //     System.Console.WriteLine("Number is 10");
+
+            // }
+            // else if (number == 20)
+            // {
+            //     System.Console.WriteLine("number is 20");
+
+            // }
+            // else if (number == 30)
+            // {
+            //     System.Console.WriteLine("number is 30");
+
+            // }
+            // else
+            // {
+            //     System.Console.WriteLine("Pls enter a number");
+            // }
+
+
+
+            //Switch Statements
+            // int number = 20;
+
+            // switch(number)
+            // {
+            //     case 10:
+            //     System.Console.WriteLine("Number is 10");
+            //     break;
+
+            //     case 20:
+            //     System.Console.WriteLine("Number is 20");
+            //     break;
+
+            //     case 30:
+            //     System.Console.WriteLine("Number is 30");
+            //     break;
+
+            //     default:
+            //      System.Console.WriteLine("Invalid");
+            //      break;
+
+           // }
+
+
+                  // While loop execution
+                    // System.Console.WriteLine("Enter a Number");
+                    // int a = int.Parse(Console.ReadLine());
+                    // //int a = 10;
+                    // while (a < 20){
+                    //     System.Console.WriteLine("Value is {0}", a);
+                    //     a++;
+                    // }
+
+
+                    //DO-While Loop
+
+                    // int a = 19;
+                    // do
+                    // {
+                    //     System.Console.WriteLine("Value is {0}", a);
+                    //     a++;
+                    // }
+                    // while (a < 20);
+
+
+                    //For Loop
+
+                    // for (int i=0; i < 5; i++)
+                    // {
+                    //     System.Console.WriteLine(i);
+                    // }
+
+
+                    //ForEach Loop
+                    string[] cars = { "Volvo", "Benz", "Mazda" };
+
+                    foreach(var car in cars)
+                    {
+                        Console.WriteLine(car);
+                    }
+
         }
     }
 }
